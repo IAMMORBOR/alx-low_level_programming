33b9cc8a2f6e32bd4c8405ea181c65f95f3fec90
Omobolanle Famotibe <ofamotibe@gmail.com>
2022-03-21 04:48:55 +0100

    the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Determines if digit is greater than 5, less than 6, or is 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
