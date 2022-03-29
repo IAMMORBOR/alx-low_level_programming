ee9ddaea444c19a62fe97740c2c450c1dac72109
Omobolanle Famotibe <ofamotibe@gmail.com>
2022-03-29 04:03:39 +0100

    adjusted files

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,20 @@
+#include <stdio.h>
 #include "main.h"
+
 /**
-*_isupper - function that checks for uppercase characters
-*@c: int type number
-*Return: 1 if uppercase, else 0
-*/
+ * _isupper - function that checks for upper case character
+ * @c:  argument of a function
+ *Return: Always 0.
+ */
 int _isupper(int c)
 {
-if ((c >= 'A') && (c <= 'Z'))
+if (c >= 'A' && c <= 'Z')
+{
 return (1);
+}
 else
+{
 return (0);
 }
+}
+
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,10 +1,25 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _SAMPLE_HEADER_H_
+#define _SAMPLE_HEADER_H_
 
-#include <stdio.h>
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
 
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-void print_remaining_days(int month, int day, int year);
-int convert_day(int month, int day);
-#endif /* MAIN_H */
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+
+
+#endif /* _SAMPLE_HEADER_H_ */
+
+
