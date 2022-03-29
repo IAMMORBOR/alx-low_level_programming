fb14967a726bf12c41794e717588ab3e3904041a
Omobolanle Famotibe <ofamotibe@gmail.com>
2022-03-29 01:18:07 +0100

    main.h changed

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,26 +1,10 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * largest_number - returns the largest of 3 numbers
- * @a: first integer
- * @b: second integer
- * @c: third integer
- * Return: largest number
- */
-int largest_number(int a, int b, int c)
-{
-int largest;
-if (a > b && b > c)
-{
-largest = a;
-}
-else if (b > a && a > c)
-{
-largest = b;
-}
-else
-{
-largest = c;
-}
-return (largest);
-}
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+#endif /* MAIN_H */
